07b5dc57caf315f9b0d7dc22a5619c509fa3eeb9
Danny Molnar <danny.molnar@cloudreach.com>
2022-11-09 16:36:55 +0000

    init commit: add new infra with tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "cr-lab-dmolnar-0911225043"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
